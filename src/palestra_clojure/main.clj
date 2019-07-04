6a3d4860f3c9f6f822b6aecbf3d61cd6f2506d80
Newton Beck <newton.beck@nubank.com.br>
2019-07-04 09:36:36 -0300

    Extrai o controller para uma função

--- src/palestra_clojure/main.clj
+++ src/palestra_clojure/main.clj
@@ -5,8 +5,11 @@
             [ring.middleware.json :refer [wrap-json-response]]
             [ring.adapter.jetty :refer [run-jetty]]))
 
+(defn listar-produtos [request]
+  {:status 200 :headers {"Content-type" "application/json"} :body {:id 1 :nome "iPhone"}})
+
 (defroutes app-routes
-  (GET "/produtos" [] {:status 200 :headers {"Content-type" "application/json"} :body {:id 1 :nome "iPhone"}})
+  (GET "/produtos" request (listar-produtos request))
   (route/not-found "<h1>Page not found</h1>"))
 
 (def handler (-> app-routes wrap-json-response))
